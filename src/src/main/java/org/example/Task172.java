0d6819b6efa5d67dcabe1d94c78469da809a822a
ZhekaFuks <faksevgenyy@gmail.com>
2025-01-03 23:50:57 +0300

    03.01.2025

---
+++ src/src/main/java/org/example/Task172.java
@@ -0,0 +1,16 @@
+package org.example;
+import java.util.Scanner;
+
+public class Task172 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int firstOne;
+        firstOne = scanner.nextInt();
+        System.out.println();
+        Scanner scanner1 = new Scanner(System.in);
+        int secondOne;
+        secondOne = scanner1.nextInt();
+        int sum = firstOne + secondOne;
+        System.out.println(firstOne+secondOne);
+    }
+}
