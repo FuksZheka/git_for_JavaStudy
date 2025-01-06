3d421200120877315aebc03c04aca47019dffb9c
ZhekaFuks <faksevgenyy@gmail.com>
2025-01-06 18:38:26 +0300

    06.01.2025 2

--- src/src/main/java/org/example/algorithm_construct/Task215.java
+++ src/src/main/java/org/example/algorithm_construct/Task215.java
@@ -1,4 +1,27 @@
 package org.example.algorithm_construct;
+import java.util.Arrays;
+import java.util.Scanner;
+
 
 public class Task215 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] team_1_marks = new int[2],team_2_marks = new int[4];
+        for (int i = 0; i < team_1_marks.length; i++) {
+            team_1_marks[i] = scanner.nextInt();
+        }
+        for (int j = 0; j < team_2_marks.length; j++) {
+            team_2_marks[j] = scanner.nextInt();
+        }
+        double team_1_average = (double) Arrays.stream(team_1_marks).sum() / team_1_marks.length;
+        double team_2_average = (double) Arrays.stream(team_2_marks).sum() / team_2_marks.length;
+        if (team_1_average > team_2_average){
+            System.out.println("First");
+        } else if (team_1_average == team_2_average) {
+            System.out.println("Draw");
+        } else if (team_1_average < team_2_average){
+            System.out.println("Second");
+        }
+
+    }
 }
--- src/src/main/java/org/example/algorithm_construct/Task217.java
+++ src/src/main/java/org/example/algorithm_construct/Task217.java
@@ -1,4 +1,27 @@
 package org.example.algorithm_construct;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Task217 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+        int hours = scanner.nextInt();
+        double tariff = scanner.nextDouble();
+        double result = 0;
+        
+        if (hours < 0 || tariff < 0) {
+            System.out.println("ERROR");
+            return;
+        }
+        if (hours <= 20) {
+            result = hours * tariff; 
+        } else if (hours > 20 && hours <= 40) {
+            result = 20 * tariff + (hours-20) * tariff * 1.5;
+        } else if (hours > 40) {
+            result = 20 * tariff + 20 * tariff * 1.5 + (hours-40) * tariff * 2;
+        }
+        System.out.printf("%.2f",result);
+    }
+
 }
