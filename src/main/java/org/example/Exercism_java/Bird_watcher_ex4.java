32e93ac01c653864682d4f4f3296e30ef12179f2
ZhekaFuks <faksevgenyy@gmail.com>
2025-06-24 01:09:37 +0300

    exercism.com Java track solves

---
+++ src/main/java/org/example/Exercism_java/Bird_watcher_ex4.java
@@ -0,0 +1,131 @@
+package org.example.Exercism_java;
+
+/*Ex3_Introduction
+Arrays
+
+In Java, arrays are a way to store multiple values of the same type in a single structure. Unlike other data structures, arrays have a fixed size once created. Elements can be assigned to an array or retrieved from it using an index. Java arrays use zero-based indexing: the first element's index is 0, the second element's index is 1, etc.
+
+Here is the standard syntax for initializing an array:
+
+type[] variableName = new type[size];
+
+The type is the type of elements in the array which may be a primitive type (e.g. int) or a class (e.g. String).
+
+The size is the number of elements this array will hold (which cannot be changed later). After array creation, the elements are initialized to their default values (typically 0, false or null).
+
+// Declare array with explicit size (size is 2)
+int[] twoInts = new int[2];
+
+Arrays can also be defined using a shortcut notation that allows you to both create the array and set its value:
+
+// Two equivalent ways to declare and initialize an array (size is 3)
+int[] threeIntsV1 = new int[] { 4, 9, 7 };
+int[] threeIntsV2 = { 4, 9, 7 };
+
+As the compiler can now tell how many elements the array will have, the length can be omitted.
+
+Array elements may be assigned and accessed using a bracketed index notation:
+
+// Assign second element by index
+twoInts[1] = 8;
+
+// Retrieve the second element by index and assign to the int element
+int secondElement = twoInts[1];
+
+Accessing an index that is outside of the valid indexes for the array results in an IndexOutOfBoundsException.
+
+Arrays can be manipulated by either calling an array instance's methods or properties, or by using the static methods defined in the Arrays class (typically only used in generic code). The length property holds the length of an array. It can be accessed like this:
+
+int arrayLength = someArray.length;
+
+Java also provides a helpful utility class java.util.Arrays that has lots of useful array-related methods (eg. Arrays.equals).
+
+Java also supports multi-dimensional arrays like int[][] arr = new int[3][4]; which can be very useful.
+
+The fact that an array is also a collection means that, besides accessing values by index, you can iterate over all its values using a for-each loop:
+
+char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+for(char vowel: vowels) {
+    // Output the vowel
+    System.out.print(vowel);
+}
+
+// => aeiou
+
+If you want more control over which values to iterate over, a for loop can be used:
+
+char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+for (int i = 0; i < 3; i++) {
+    // Output the vowel
+    System.out.print(vowels[i]);
+}
+
+// => aei
+
+Instructions
+
+You're an avid bird watcher who keeps track of how many birds have visited your garden in the last seven days.
+
+You have six tasks, all dealing with the numbers of birds that visited your garden.
+*/
+public class Bird_watcher_ex4 {
+    private final int[] birdsPerDay;
+
+    public Bird_watcher_ex4(int[] birdsPerDay) {
+        this.birdsPerDay = birdsPerDay.clone();
+    }
+
+    public int[] getLastWeek() {
+        return new int[]{0, 2, 5, 3, 7, 8, 4};
+    }
+
+    public int getToday() {
+        return this.birdsPerDay[birdsPerDay.length - 1];
+    }
+
+    public void incrementTodaysCount() {
+        this.birdsPerDay[birdsPerDay.length - 1] = getToday() + 1;
+    }
+
+    public boolean hasDayWithoutBirds() {
+        for (int countOfBirds : this.birdsPerDay) {
+            if (countOfBirds == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getCountForFirstDays(int numberOfDays) {
+        int result = 0;
+        if (numberOfDays > birdsPerDay.length) {
+            numberOfDays = birdsPerDay.length;
+        }
+        for (int i = 0; i < numberOfDays; i++) {
+            result += this.birdsPerDay[i];
+        }
+        return result;
+    }
+
+    public int getBusyDays() {
+        int countOfBusyDays = 0;
+        for (int i = 0; i < birdsPerDay.length; i++) {
+            if (this.birdsPerDay[i] >= 5) {
+                countOfBusyDays += 1;
+            }
+        }
+        return countOfBusyDays;
+    }
+
+    public static void main(String[] args) {
+        int[] birdsPerDay = {2, 5, 0, 7, 4, 1};
+        Bird_watcher_ex4 birdCount = new Bird_watcher_ex4(birdsPerDay);
+//        birdCount.incrementTodaysCount();
+//        System.out.println(birdCount.getToday());
+        System.out.println(birdCount.getBusyDays());
+        ;
+// => 2
+    }
+}
---
+++ src/main/java/org/example/Exercism_java/Karls_Languages_ex5.java
@@ -0,0 +1,108 @@
+package org.example.Exercism_java;
+/*Introduction
+Generic Types
+
+A generic type is a generic class or interface that is parameterized over types. This allows the compiler to enforce type safety on the class or interface.
+
+Consider this non-generic Container interface:
+
+class Container {
+    private Object object;
+
+    public void set(Object object) { this.object = object; }
+    public Object get() { return object; }
+}
+
+Since it accepts and returns Object types, it works with any non-primitive type. However, this comes at a cost because some code may call get expecting Integers while other code calls set adding Strings resulting in a runtime exception.
+
+A generic class and generic interface have the following formats:
+
+class ClassName<T1, T2, ..., Tn> { ... }
+interface InterfaceName<T1, T2, ..., Tn> { ... }
+
+The type parameter section, delimited by angle brackets (<>), following the class or interface name specifies the type parameters (also called type variables) T1, T2, ..., and Tn. These can be used inside the body of the class or interface to get generic type safety.
+
+Here is the generic version of Container:
+
+class Container<E> {
+    private E object;
+
+    public void set(E object) { this.object = object; }
+    public E get() { return object; }
+}
+
+        When created, now we have to declare what type it holds and the compiler will enforce that constraint:
+
+// empty <> can infer from context
+Container<String> stringContainer = new Container<>();
+// compiler knows this is a String, so it is allowed
+stringContainer.set("Some string");
+// no cast needed, compiler knows it is a String
+String result = stringContainer.get();
+// this causes a compiler error:
+stringContainer.set(42);
+
+Lists
+
+Lists are the ordered sequence collection in Java. Unlike arrays, a List can grow in size to accommodate any number of items. One standard implementation is the ArrayList which is backed by a re-sizable array. Another standard implementation is the LinkedList class which is backed by a doubly-linked list.
+
+Lists may be empty or hold any number of items (including duplicates). Lists are a generic interface typed to indicate which type of objects they can contain. For example:
+
+List<String> emptyListOfStrings = List.of();
+List<Integer> singleInteger = List.of(1);
+List<Boolean> threeBooleans = List.of(true, false, true);
+List<Object> listWithMultipleTypes = List.of("hello", 1, true);
+
+Lists have various helpful methods to add, remove, get, and check for an element to be present:
+
+List<Character> vowels = new ArrayList<>(List.of('a', 'e', 'i', 'o', 'i', 'e', 'a'));
+int startingSize = vowels.size(); // 7
+vowels.add('u'); // vowels is now ['a', 'e', 'i', 'o', 'i', 'e', 'a', 'u']
+char a = vowels.get(0); // 'a'
+boolean hadI = vowels.remove('i'); // true and vowels is now ['a', 'e', 'o', 'i', 'e', 'a', 'u']
+boolean hasI = vowels.contains('i'); // true (still have one more left)
+
+Instructions
+
+Karl wants to keep track of a list of languages to learn on Exercism's website. Karl needs to be able to add new languages, remove old ones and check if certain languages are in the list. It would be very exciting if Karl wants to learn Java or Kotlin!
+        */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Karls_Languages_ex5 {
+    private final List<String> languages = new ArrayList<>();
+
+    public boolean isEmpty() {
+        return languages.isEmpty();
+    }
+
+    public void addLanguage(String language) {
+        this.languages.add(language);
+    }
+
+    public void removeLanguage(String language) {
+        this.languages.remove(language);
+    }
+
+    public String firstLanguage() {
+        return this.languages.getFirst();
+    }
+
+    public int count() {
+        return this.languages.size();
+    }
+
+    public boolean containsLanguage(String language) {
+        return this.languages.contains(language);
+    }
+
+    public boolean isExciting() {
+        return containsLanguage("Java") || containsLanguage("Kotlin");
+    }
+
+    public static void main(String[] args) {
+        Karls_Languages_ex5 karlsLanguagesEx5 = new Karls_Languages_ex5();
+        System.out.println(karlsLanguagesEx5.isEmpty());
+    }
+}
